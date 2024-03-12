3fe8229eccb7a587f6ab038ca52d2f2eec63a6b6
jimmyverdoora <micheleroselli94@gmail.com>
2024-03-12 17:58:44 +0100

    added mobile

--- web/src/App.js
+++ web/src/App.js
@@ -1,3 +1,5 @@
+import './App.css';
+
 import React from 'react';
 import Home from './Home';
 import Dungeon from './Dungeon';
@@ -10,7 +12,17 @@ function App() {
 
   return (
     <div style={{ width: '100vw', minHeight: '100vh', backgroundColor: 'black' }}>
-      {status == 'connected' && chainId == CHAIN_ID ? <Dungeon /> : <Home />}
+      <div className='desktop'>
+        {status == 'connected' && chainId == CHAIN_ID ? <Dungeon /> : <Home />}
+      </div>
+      <div className='mobile'>
+        <div style={{ color: '#ffd480', fontFamily: 'monospace', display: 'flex', justifyContent: 'center' }}>
+          <p style={{ marginTop: 0, paddingTop: '30px', fontSize: 'large' }}>You can't play from mobile.</p>
+        </div>
+        <div style={{ color: '#ffd480', fontFamily: 'monospace', display: 'flex', justifyContent: 'center' }}>
+          <p style={{ fontSize: 'large', marginLeft: '20px', marginRight: '20px' }}>Get a laptop or PC. If you don't have money, why are you even on a crypto gambling game website?!</p>
+        </div>
+      </div>
     </div>
   );
 }
