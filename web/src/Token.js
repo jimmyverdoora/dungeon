45bc19aa14bfe6d9580ee4e0d067607dad774d9e
Kevin K. <kevin.klammer@gmail.com>
2024-03-26 19:21:39 +0100

    small changes on ICO page...

--- web/src/Token.js
+++ web/src/Token.js
@@ -56,20 +56,21 @@ function Token() {
       <Title />
       <div style={{ display: 'flex', justifyContent: 'center', fontSize: 'large' }}>
         <div>
-          <p style={{ marginBottom: 0, marginTop: 0 }}>+--- Why $DUNG? </p>
-          <p style={{ marginBottom: 0, marginTop: 0 }}>| Play money is 100% MATIC. The $DUNG token</p>
-          <p style={{ marginBottom: 0, marginTop: 0 }}>| lets you "own" your stake of the game.</p>
-          <p style={{ marginBottom: 0, marginTop: 0 }}>| And this is not only for chapter 1 but for</p>
-          <p style={{ marginBottom: 0, marginTop: 0 }}>| all chapters to come. For chapter 1, 10%</p>
-          <p style={{ marginBottom: 0, marginTop: 0 }}>| of all in game money gets distributed to</p>
-          <p style={{ marginBottom: 0, marginTop: 0 }}>| the $DUNG hodlers, increasing the MATIC</p>
-          <p style={{ marginBottom: 0, marginTop: 0 }}>| value in the liquidity pool.</p>
-          <p style={{ marginBottom: 0, marginTop: 0 }}>| NOW JOIN THE ICO BEFORE IT ENDS!!!</p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>+--- $DUNG Token ICO - own part of the game.</p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>|</p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>| Each Chapter, $DUNG holders will get ≈30%</p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>|  of the full payout of the game. </p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>|</p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>| Our estimation is a market cap of around </p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>| $100M within 6 months and $1B long term.</p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>|</p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>| Get $DUNG, click <span style={{ color: '#fc4521' }}>'LOCK MATIC'</span> below. </p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>|</p>
         </div>
       </div>
       <div style={{ display: 'flex', justifyContent: 'center', fontSize: 'large', color: '#4aff47', backgroundColor: 'black', }}>
         <div>
-          <p style={{ marginBottom: 0, marginTop: 0 }}>|</p>
+
           <p style={{ marginBottom: 0, marginTop: 0 }}>+--- Your position</p>
           <p style={{ marginBottom: 0, marginTop: 0 }}>| {account}</p>
           <p style={{ marginBottom: 0, marginTop: 0 }}>| Balance: {balance} MATIC</p>
@@ -77,32 +78,37 @@ function Token() {
           <p style={{ marginBottom: 0, marginTop: 0 }}>+-------------------------------------------</p>
           <p style={{ marginBottom: 0, marginTop: 0 }}>| Total ICO value locked: {icoValueTot} MATIC</p>
           <p style={{ marginBottom: 0, marginTop: 0 }}>+-------------------------------------------</p>
-          <p style={{ marginBottom: 0, marginTop: 0 }}>| <a href="#" style={{ textDecoration: 'none', color: '#ffd480' }} onClick={() => setShowOpen(true)} >LOCK MATIC</a>{'\u00A0'.repeat(27)}<a href="/" style={{ textDecoration: 'none', color: '#ffd480' }} >BACK</a></p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>|</p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>| <a href="#" style={{ textDecoration: 'none', color: '#fc4521' }} onClick={() => setShowOpen(true)} >LOCK MATIC</a>{'\u00A0'.repeat(27)}<a href="/" style={{ textDecoration: 'none', color: '#ffd480' }} >BACK</a></p>
+          <p style={{ marginBottom: 0, marginTop: 0 }}>|</p>
           <p style={{ marginBottom: 0, marginTop: 0 }}>+-------------------------------------------</p>
         </div>
       </div>
       {showOpen && <div style={{ zIndex: 1, color: '#fc4521', backgroundColor: 'black', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', fontFamily: 'monospace', fontSize: 'large' }}>
         <p style={{ marginBottom: 0, marginTop: 0 }}>+------------------------------------------------------------+</p>
-        <p style={{ marginBottom: 0, marginTop: 0 }}>|{'\u00A0'.repeat(25)}<span style={{ color: '#ffd480' }}>BUY $DUNG</span>{'\u00A0'.repeat(26)}|</p>
+        <p style={{ marginBottom: 0, marginTop: 0 }}>|{'\u00A0'.repeat(25)}<span style={{ color: '#ffd480' }}>GET $DUNG</span>{'\u00A0'.repeat(26)}|</p>
+        <p style={{ marginBottom: 0, marginTop: 0 }}>| {'\u00A0'.repeat(58)} |</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>| 30% of the total supply will be distributed proportionally{'\u00A0'}|</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>| to the ICO participants. Once the limit block is reached,{'\u00A0'.repeat(2)}|</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>| all the MATIC locked will be added in the token liquidity{'\u00A0'.repeat(2)}|</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>| pool, $DUNG token given out to ICO members and buy/sell{'\u00A0'.repeat(4)}|</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>| functionality of $DUNG token enabled. The swap pool is{'\u00A0'.repeat(5)}|</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>| embedded inside the token contract.{'\u00A0'.repeat(24)}|</p>
+        <p style={{ marginBottom: 0, marginTop: 0 }}>| {'\u00A0'.repeat(58)} |</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>| TOKEN: <a href={`https://polygonscan.com/token/${TOKEN_ADDRESS.toLowerCase()}`} style={{ textDecoration: 'none', color: '#ffd480' }} >{TOKEN_ADDRESS}</a>{'\u00A0'.repeat(10)}|</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>| ICO: <a href={`https://polygonscan.com/address/${ICO_ADDRESS.toLowerCase()}`} style={{ textDecoration: 'none', color: '#ffd480' }} >{ICO_ADDRESS}</a>{'\u00A0'.repeat(12)}|</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>|{'\u00A0'.repeat(60)}|</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>|{'\u00A0'.repeat(22)}<span style={{ color: '#ffd480' }}>+-------------+</span>{'\u00A0'.repeat(23)}|</p>
-        <p style={{ marginBottom: 0, marginTop: 0 }}>|{'\u00A0'.repeat(22)}<span style={{ color: '#ffd480' }}>|</span><input
+        <p style={{ marginBottom: 0, marginTop: 0 }}>{'\u00A0'.repeat(23)}<span style={{ color: '#ffd480' }}>|</span><input
           type="number"
           style={{ width: '106px', fontFamily: 'monospace', color: '#47fffc', border: 'none', background: 'none', outline: 'none' }}
           value={value}
           onChange={e => setValue(Number(e.target.value))}
           onWheel={() => {}}
           autoFocus={true}
-        /><span style={{ color: '#ffd480' }}>|</span>{'\u00A0'.repeat(23)}|</p>
+        /><span style={{ color: '#ffd480' }}>{'\u00A0'.repeat(2)}|</span>{'\u00A0'.repeat(22)} </p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>|{'\u00A0'.repeat(22)}<span style={{ color: '#ffd480' }}>+-------------+</span>{'\u00A0'.repeat(23)}|</p>
+        <p style={{ marginBottom: 0, marginTop: 0 }}>| {'\u00A0'.repeat(58)} |</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>| <a href="#" style={{ textDecoration: 'none', color: '#ffd480' }} onClick={() => sendValue()}>LOCK!</a>{'\u00A0'.repeat(48)}<a href="#" style={{ textDecoration: 'none', color: '#ffd480' }} onClick={() => setShowOpen(false)}>CLOSE</a> |</p>
         <p style={{ marginBottom: 0, marginTop: 0 }}>+------------------------------------------------------------+</p>
       </div>}
